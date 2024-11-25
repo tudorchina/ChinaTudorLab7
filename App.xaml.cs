00c6c85d0a9fbfb5839d152850b871d1559e1c12
China Tudor <tudorchina@yahoo.com>
2024-11-25 15:09:42 +0200

    lab8

--- App.xaml.cs
+++ App.xaml.cs
@@ -1,7 +1,23 @@
-﻿namespace ChinaTudorLab7
+﻿using System;
+using ChinaTudorLab7.Data;
+using System.IO;
+namespace ChinaTudorLab7
 {
     public partial class App : Application
     {
+        static ShoppingListDatabase database;
+
+        public static ShoppingListDatabase Database
+        {
+            get
+            {
+                if (database == null)
+                {
+                    database = new ShoppingListDatabase(Path.Combine(Environment.GetFolderPath(Environment.SpecialFolder.LocalApplicationData), "ShoppingList.db3"));
+                }
+                return database;
+            }
+        }
         public App()
         {
             InitializeComponent();
